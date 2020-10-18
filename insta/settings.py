9841e34cdcd8d96b9887f3d815e3842c46b52885
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-10-18 21:03:59 +0300

    initiazizede db in settings

--- insta/settings.py
+++ insta/settings.py
@@ -23,7 +23,14 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 if config('MODE')=="dev":
    DATABASES = {
        'default': {
-
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
    }
 # production
 else:
